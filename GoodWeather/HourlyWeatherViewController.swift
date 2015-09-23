929ba9d8252605c7ac714398fac5821a2759a0bc
Amy Schmidt <xaschmidtx@gmail.com>
2015-09-23 18:02:31 -0500

    Table Views Setup

--- GoodWeather/HourlyWeatherViewController.swift
+++ GoodWeather/HourlyWeatherViewController.swift
@@ -8,28 +8,24 @@
 
 import UIKit
 
-class HourlyWeatherViewController: UIViewController {
+class HourlyWeatherViewController: UIViewController, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-    */
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let hourCell = tableView.dequeueReusableCellWithIdentifier("HourCell", forIndexPath: indexPath)
+        
+        hourCell.textLabel?.text = "Hello"
+        hourCell.selectionStyle = .None
+        
+        return hourCell
+    }
 
 }
--- TenDayViewController.swift
+++ TenDayViewController.swift
@@ -8,28 +8,23 @@
 
 import UIKit
 
-class TenDayViewController: UIViewController {
+class TenDayViewController: UIViewController, UITableViewDataSource {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
-    */
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let dayCell = tableView.dequeueReusableCellWithIdentifier("DayCell", forIndexPath: indexPath)
+        
+        dayCell.textLabel?.text = "Hello"
+        dayCell.selectionStyle = .None
+        
+        return dayCell
+    }
 }
