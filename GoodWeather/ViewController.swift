311b760087dbc59c435849ae4de2ed5779f9e550
Amy Schmidt <xaschmidtx@gmail.com>
2015-09-23 16:33:51 -0500

    Initial Commit

---
+++ GoodWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoodWeather
+//
+//  Created by Amy Schmidt on 9/23/15.
+//  Copyright © 2015 Amy Schmidt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
